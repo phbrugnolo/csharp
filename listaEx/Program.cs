6a9be50d6524d3f068d97fad39c5df278ff18b7e
phbrugnolo <phbrugnolo@gmail.com>
2024-03-11 20:37:37 -0300

    Adicionando Projeto De Ordenação de Vetores

---
+++ listaEx/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Vetor de números inteiros não ordenado
+        int[] vetor = { 5, 2, 9, 1, 7 };
+
+        // Ordena o vetor
+        Array.Sort(vetor);
+
+        // Exibe o vetor ordenado
+        Console.WriteLine("Vetor ordenado:");
+        foreach (int num in vetor)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
