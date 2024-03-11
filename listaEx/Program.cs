b259e3627fea48b1a91599a530689bfe726612c5
phbrugnolo <phbrugnolo@gmail.com>
2024-03-11 20:51:20 -0300

    Adiconando a função de criar um array com números aleatórios

--- listaEx/Program.cs
+++ listaEx/Program.cs
@@ -4,15 +4,30 @@ class Program
 {
     static void Main()
     {
-        // Vetor de números inteiros não ordenado
-        int[] vetor = { 5, 2, 9, 1, 7 };
+
+        Random rand = new Random();
+
+        int randomNumber = rand.Next(0, 101);
+
+        // Tamanho da array
+        int tamanhoArray = 100;
+
+        // Array para armazenar os números aleatórios
+        int[] numerosAleatorios = new int[tamanhoArray];
+
+        // Preenche a array com números aleatórios
+        for (int i = 0; i < tamanhoArray; i++)
+        {
+            numerosAleatorios[i] = rand.Next(100, 501);
+        }
+
 
         // Ordena o vetor
-        Array.Sort(vetor);
+        Array.Sort(numerosAleatorios);
 
         // Exibe o vetor ordenado
         Console.WriteLine("Vetor ordenado:");
-        foreach (int num in vetor)
+        foreach (int num in numerosAleatorios)
         {
             Console.WriteLine(num);
         }
